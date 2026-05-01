c628b327790ce3c8d1b6f0e9a0de3dc8b9450bdc
Evaristo Barauna <falcaozitos3@gmail.com>
2026-05-01 18:54:07 -0300

    feat: organiza estrutura de arquivos e adiciona pagina principal

--- DesenvolvimentoSistema/Contatar.py
+++
@@ -1,5 +0,0 @@
-# contato, onde o usuário pode entrar em contato com a adiministração do site, ou com os prprios devs.
-
-
-from flask import Flask, render_template, request, redirect, url_for
-from main import app
--- DesenvolvimentoSistema/entrar.py
+++
@@ -1,4 +0,0 @@
-#onde o usuário pode criar uma conta, para acessar as funcionalidades do site, como criar projetos, etc.
-from flask import Flask, render_template, request, redirect, url_for
-from main import app
-
