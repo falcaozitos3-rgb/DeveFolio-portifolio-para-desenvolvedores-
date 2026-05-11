a6df60ad964db25936e690f2c9a8edb0b4847345
Evaristo Barauna <falcaozitos3@gmail.com>
2026-05-11 08:25:49 -0300

    banco de dados adicionados co susseço

--- DesenvolvimentoSistema/bancodedados.py
+++ DesenvolvimentoSistema/bancodedados.py
@@ -2,5 +2,39 @@
 # por enquanto, estamos usando um arquivo JSON para simular um banco de dados simples, mas em um projeto real.
 # você pode usar um banco de dados relacional como MySQL, PostgreSQL ou SQLite.
 
-from flask import Flask, render_template, request, redirect, url_for
-from main import app
+import sqlite3
+def criar_banco():
+    conexao = sqlite3.connect("devefolio.db")
+
+    cursor = conexao.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT,
+            email TEXT,
+            senha TEXT,
+            telefone TEXT
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS projetos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT,
+            descricao TEXT,
+            linguagem TEXT,
+            autor_email TEXT
+        )
+    ''')
+
+    conexao.commit()
+    conexao.close()
+
+criar_banco()
+
+
+def get_conexao():
+    conexao = sqlite3.connect("devefolio.db")
+    conexao.row_factory = sqlite3.Row
+    return conexao
+
